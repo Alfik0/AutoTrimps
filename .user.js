2528f1c832f09cd7d3cbba54f9a0fb8e47772a83
Alfik0 <johnpin444@gmail.com>
2020-06-22 21:22:01 +0200

    claim fork ownership

--- .user.js
+++ .user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
-// @version      1.0-Zek
+// @name         AutoTrimps-Alfik0
+// @version      1.0-Alfik0
 // @namespace    https://Zorn192.github.io/AutoTrimps
 // @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Alfik0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
 // @connect      *Zorn192.github.io/AutoTrimps*
@@ -14,9 +14,9 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-Alfik0';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://Alfik0.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
